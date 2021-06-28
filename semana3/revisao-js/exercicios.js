8460e1d673194f82b07dcc95addd339a316d0e47
camilagsoares <camila.soares19@hotmail.com>
2021-06-28 16:19:39 -0300

    Fiz o exercício 5

---
+++ semana3/revisao-js/exercicios.js
@@ -0,0 +1,159 @@
+// EXERCÍCIO 01
+function inverteArray(array) {
+  let array = [10,9,5,7]
+  return
+}
+
+// EXERCÍCIO 02
+function retornaNumerosParesElevadosADois(array) {
+arrayNumeros = [1,4,7,9]
+let novoArrayNumeros = arrayNumeros * 2
+console.log(novoArrayNumeros)
+
+
+
+ }
+ retornaNumerosParesElevadosADois()
+
+// EXERCÍCIO 03
+function retornaNumerosPares(array) {
+
+}
+
+// EXERCÍCIO 04
+function retornaMaiorNumero(array) {
+  let arrayDeNumeros = [1,2,10,90]
+   return array.max()
+
+} retornaMaiorNumero()
+
+// EXERCÍCIO 05
+function retornaQuantidadeElementos(array) {
+  let arrayNumeros = [1,2,5]
+  return array.length
+}
+
+retornaQuantidadeElementos()
+
+// EXERCÍCIO 06
+function retornaExpressoesBooleanas() {
+const booleano1 = true
+const booleano2 = false
+const booleano3 = !booleano2 //true
+const booleano4 = !booleano3 //false
+
+const pergunta1= booleano1 && booleano2 && !booleano4
+const pergunta2 = (booleano1 && booleano2) || !booleano3
+const pergunta3 = (booleano2 || booleano3) && (booleano4 && booleano3)
+const pergunta4 = !(booleano2 && booleano3) || !(booleano1 && booleano3)
+const pergunta5 = !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+
+const respostas = [pergunta1,pergunta2,pergunta3,pergunta4,pergunta5]
+console.log(respostas)
+ }
+  retornaExpressoesBooleanas()
+
+// EXERCÍCIO 07
+function retornaNNumerosPares(n) {
+
+}
+
+// EXERCÍCIO 08
+function checaTriangulo(a, b, c) {
+  
+  // return 'Escaleno'
+  // return 'Equilátero'
+  // return 'Isósceles'
+}
+
+// EXERCÍCIO 09
+function comparaDoisNumeros(num1, num2) {
+  let num1 = 10
+  let num2 = 4
+  // Formato do objeto a ser retornado:
+  // {
+  //   maiorNumero: X,
+  //   maiorDivisivelPorMenor: Y,
+  //   diferenca: Z
+  // }
+}
+
+// EXERCÍCIO 10
+function segundoMaiorEMenor(array) {
+
+}
+
+// EXERCÍCIO 11
+function ordenaArray(array) {
+
+}
+
+// EXERCÍCIO 12
+function filmeFavorito() {
+
+}
+
+// EXERCÍCIO 13
+function imprimeChamada() {
+  // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+}
+
+// EXERCÍCIO 14
+function criaRetangulo(lado1, lado2) {
+
+}
+
+// EXERCÍCIO 15
+function anonimizaPessoa(pessoa) {
+
+}
+
+// EXERCÍCIO 16A
+function maioresDe18(arrayDePessoas) {
+
+}
+
+// EXERCÍCIO 16B
+function menoresDe18(arrayDePessoas) {
+
+}
+
+// EXERCÍCIO 17A
+function multiplicaArrayPor2(array) {
+
+}
+
+// EXERCÍCIO 17B
+function multiplicaArrayPor2S(array) {
+
+}
+
+// EXERCÍCIO 17C
+function verificaParidade(array) {
+
+}
+
+// EXERCÍCIO 18A
+function retornaPessoasAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 18B
+function retornaPessoasNaoAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 19A
+function ordenaPorNome(consultasNome) {
+
+}
+
+// EXERCÍCIO 19B
+function ordenaPorData(consultasData) {
+
+}
+
+// EXERCÍCIO 20
+function calculaSaldo(contas) {
+
+}
