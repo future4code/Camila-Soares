ce76c7f5658c8fef4efe976cf50094dfac7c96c9
camilagsoares <camila.soares19@hotmail.com>
2021-08-10 13:59:12 -0300

    Comecei a fazer o exercício de use-effect

---
+++ semana9/aula32-use-effect/use-effect/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import axios from "axios";
+import PokeCard from "./PokeCard/index";
+
+
+function App () {
+// const [pokeList,setPokeList] = useState([])
+
+const [pokeName,setPokeName] = useState('')
+
+  const procuraListaPokemon = () => {
+    const response = axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`) 
+    .then(response => setPokeName(response.data))
+    .catch(error => console.log(error))
+  }
+
+  useEffect( () => {
+    procuraListaPokemon()
+  },[])
+
+
+
+return(
+  <div>
+    <h1>Lista de pokemon</h1>
+    </div>
+)
+}
+
+export default App
