ceeab8358d748025c03e2b92abc8182fee17f9ea
camilagsoares <camila.soares19@hotmail.com>
2021-09-20 17:57:16 -0300

    Exercício módulo back end

---
+++ semana15/template-node-npm/index.js
@@ -0,0 +1,45 @@
+// const countries = require("./countries")
+// console.log(countries)
+
+
+//PERGUNTA 1
+//a) Para acessar os parâmetros passados na linha de comando para o node, digitamos o comando node index.js e o o argumento, o argumento deve ser passado a partir do índice 2.
+// b)
+ 
+//c) 
+
+const nome = "Camila"
+const idade = 21
+const idadeNoFuturo = idade + 7
+console.log(`Olá ${nome} você tem ${idade} anos`)
+console.log(`Você tem ${idade} anos. Em sete anos você terá ${idadeNoFuturo}`)
+
+
+//PERGUNTA 2
+
+// const numberOne = 3
+// const numberTwo = 5
+// const soma = numberOne + numberTwo
+// const subt = numberTwo * numberOne
+// console.log(subt)
+// console.log(soma)
+
+
+// process.argv[2] = soma
+
+// switch(soma){
+// 	case "soma":
+// 		2 * 5
+// 		break;
+// 	case "subt":
+// 		//seu código aqui
+// 		break;
+// }
+
+// console.log(soma)
+
+//PERGUNTA 3
+
+
+// console.log(tarefas)
+// const recebeString = "Estudar Node e package.json a tarde"
