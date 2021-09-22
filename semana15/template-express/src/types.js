fbe75db317c45e40a1e4c69d83cb6d06c13e8f9c
camilagsoares <camila.soares19@hotmail.com>
2021-09-22 16:16:16 -0300

    Exercícios express

---
+++ semana15/template-express/src/types.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.CONTINENTS = void 0;
+var CONTINENTS;
+(function (CONTINENTS) {
+    CONTINENTS["AFRICA"] = "\u00C1frica";
+    CONTINENTS["AMERICA"] = "Am\u00E9rica";
+    CONTINENTS["ASIA"] = "\u00C1sia";
+    CONTINENTS["EUROPE"] = "Europa";
+    CONTINENTS["OCEANIA"] = "Oceania";
+})(CONTINENTS = exports.CONTINENTS || (exports.CONTINENTS = {}));
